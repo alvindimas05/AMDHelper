019f6d02a09599c5f8bc4390d5614d92982a4094
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-10-20 19:48:51 +0700

    fix: missing local bin folder

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ curl -sSL https://github.com/alvindimas05/AMDHelp/releases/latest/download/amdhe
 tar -xvzf amdhelper.gz
 rm amdhelper.gz
 chmod +x amdhelper
+sudo mkdir -p /usr/local/bin
 sudo mv amdhelper /usr/local/bin/amdhelper
 echo "AMDHelper installed! Run the command below to start."
 echo "sudo amdhelper"
